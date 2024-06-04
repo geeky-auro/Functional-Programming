b206499e4d3b7f9a0ef0ecfa49599c8fe72eb3f3
geeky-auro <aurosaswat.apk@gmail.com>
2024-06-05 00:32:02 +0530

    Functional Indepth

--- src/FP04CustomClass.java
+++ src/FP04CustomClass.java
@@ -1,5 +1,8 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 class Course{
     private String name;
@@ -64,6 +67,46 @@ public class FP04CustomClass {
         list.add(new Course("AWS","Cloud",92,30000));
         list.add(new Course("Azure","CLoud",92,20000));
         list.add(new Course("GCP","Cloud",93,40000));
+
+        // All Match, none match , anymatch
+        // Checking whether the review of the course is greater than 90.
+        System.out.println(list.stream().allMatch(course -> course.getReviewScore()>90));
+        System.out.println(list.stream().allMatch(course -> course.getReviewScore()>95));
+        System.out.println(list.stream().noneMatch(course -> course.getReviewScore()>90));
+
+        Predicate<Course> reviewScoreGreaterThan95 = course -> course.getReviewScore()>95;
+        Predicate<Course> reviewScoreGreaterThan90 = course -> course.getReviewScore()>90;
+
+        Predicate<Course> reviewScoreLessThan90 = course -> course.getReviewScore()<90;
+
+        System.out.println(list.stream().allMatch(reviewScoreGreaterThan95));
+        System.out.println(list.stream().noneMatch(reviewScoreGreaterThan90));
+        System.out.println(list.stream().noneMatch(reviewScoreLessThan90));
+        System.out.println(list.stream().anyMatch(reviewScoreGreaterThan95));
+
+    /*
+        Few Utility Functions present in the class !
+        allMatch() -> returns true if every element matches the predicate in the stream
+        noneMatch() -> returns true if no element matches the condition specified in the predicate
+        anyMatch() -> If atleast one element in the stream matches the specific condition.
+     */
+
+        /*
+        * Sorting using Comparator
+        * */
+        Comparator<Course> comparatorByNoOfStudents = Comparator.comparing(Course::getNoOfStudents);
+        System.out.println(list.stream().sorted(comparatorByNoOfStudents).collect(Collectors.toList()));
+        Comparator<Course> comparatorByNoOfStudentsDecreasing =Comparator.comparing(Course::getNoOfStudents).reversed();
+        System.out.println(list.stream().sorted(comparatorByNoOfStudentsDecreasing).collect(Collectors.toList()));
+
+        // Compare by No. of Students and No. of Reviews ;)
+        // Note there are default primitive methods so use primitive methods
+        // such as comparingInt so that less auto boxing will be required !
+        Comparator<Course> compareByStudentsandReviews = Comparator
+                .comparingInt(Course::getNoOfStudents)
+                .thenComparing(Course::getReviewScore);
+
+        // Skip ,limit, takeWhile and dropWhile
 
 
 
